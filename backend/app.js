740a023e48c990fc0f2f93bce7d8a9ce8b504b88
filcorpfr <filcorpfr@gmail.com>
2024-09-10 11:31:47 +0000

    test

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ app.use(express.json())
 app.get("api/deauthorize", (req,res)=>{
     const useId = req.body.user_id
     if(useId){
-        console.log("L'utilisateur avec l'ID ${userId} a révoqué l'autorisation")
+        console.log("L'utilisateur avec l'ID ${userId} a révoqué l'autorisation !")
         res.status(200).send("success")
     }else{
         res.status(400).send("error")
